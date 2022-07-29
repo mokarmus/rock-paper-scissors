4a16cd10d4083ede87f786376996d909b2a663f5
mokarmus <mateusz.okarmus.ext@iceservices.com>
2022-07-29 16:27:48 +0200

    implement first solution

--- src/main/scala/ice/mobbin/session/RockPaperScissors.scala
+++ src/main/scala/ice/mobbin/session/RockPaperScissors.scala
@@ -2,14 +2,23 @@ package ice.mobbin.session
 
 class RockPaperScissors {
 
-  def playRound(g1: Gesture, g2: Gesture): RoundResult = ???
+  def playRound(g1: Gesture, g2: Gesture): RoundResult = g1.compete(g2)
 }
 
-trait Gesture
+sealed trait Gesture {
+  def compete(other: Gesture): RoundResult
+}
 
-object Rock extends Gesture
-
-object Scissors extends Gesture
+object Rock extends Gesture {
+  override def compete(other: Gesture): RoundResult = other match {
+    case Scissors => FirstWins
+  }
+}
+object Scissors extends Gesture {
+  override def compete(other: Gesture): RoundResult = other match {
+     case Rock => SecondWins
+  }
+}
 
 trait RoundResult
 
--- src/test/scala/ice/mobbin/session/RockPaperScissorsSpec.scala
+++ src/test/scala/ice/mobbin/session/RockPaperScissorsSpec.scala
@@ -6,11 +6,13 @@ import org.scalatest.wordspec.AnyWordSpec
 class RockPaperScissorsSpec extends AnyWordSpec with Matchers {
 
   var rockPaperScissors: RockPaperScissors = new RockPaperScissors
-  "As a player" +
-    "I want rock to beat scissors" +
-    "So that I can play with rules I'm familiar with" in {
+  "As a player" should {
+    
+    "I want rock to beat scissors" in {
     rockPaperScissors.playRound(Rock, Scissors) shouldBe FirstWins
     rockPaperScissors.playRound(Scissors, Rock) shouldBe SecondWins
+      
+  } 
 
   }
 
