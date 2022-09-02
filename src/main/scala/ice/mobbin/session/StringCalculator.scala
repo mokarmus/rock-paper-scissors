c8069adc838d50985bcd5f5576c5ba9b328b0594
mokarmus <mateusz.okarmus.ext@iceservices.com>
2022-09-02 16:34:21 +0200

    the next implementation finished

--- src/main/scala/ice/mobbin/session/StringCalculator.scala
+++ src/main/scala/ice/mobbin/session/StringCalculator.scala
@@ -3,11 +3,15 @@ package ice.mobbin.session
 import java.security.InvalidParameterException
 
 object StringCalculator {
+
+  private val inputPattern = "(//)(.*)(\\n)(.*)".r
   
   def add(input: String): Int = input match {
     case "" => 0
     case invalid if invalid.matches(".*[,|\n]") => throw new InvalidParameterException()
-    case _ => input.split(",|\n").map(_.toInt).sum
+    case inputPattern(_, separator, _, theRest) => 
+      theRest.split(separator).filterNot(_ == separator).map(_.toInt).sum
+    case _ => input.split("[,\n]").map(_.toInt).sum
   }
 
 }
