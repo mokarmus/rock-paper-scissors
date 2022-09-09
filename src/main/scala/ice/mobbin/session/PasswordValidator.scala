bb3e94353505be486124298c07ae515fdb5328fa
mokarmus <mateusz.okarmus.ext@iceservices.com>
2022-09-09 16:48:07 +0200

    small fixes

--- src/main/scala/ice/mobbin/session/PasswordValidator.scala
+++ src/main/scala/ice/mobbin/session/PasswordValidator.scala
@@ -3,20 +3,29 @@ package ice.mobbin.session
 object PasswordValidator {
 
   def validate(input: String): ValidationResult = {
-    val result = Seq(inputLengthValidation, digitValidation ).flatMap(validation => validation(input)).mkString("\n")
+    val validationResult = Seq(inputLengthValidation, digitValidation).flatMap(validation => validation(input)).mkString("\n")
     
-    if (result.isEmpty) ValidationResult(true, None)
-    else ValidationResult(false, Some(result))
+    validationResult match {
+      case "" => ValidationResult.valid
+      case errors => ValidationResult.invalid(errors)
+    }
   }
   
-  val inputLengthValidation: String => Option[String] = input =>  if(input.length < 8) Some("Password must be at least 8 characters") else None
+  val inputLengthValidation: String => Option[String] = input => if(input.length < 8) Some("Password must be at least 8 characters") else None
   
   val digitValidation: String => Option[String] = input => if (input.count(_.isDigit) < 2) Some("The password must contain at least 2 numbers") else None
+  
 }
 
-
-
 case class ValidationResult(isValid:Boolean, validationError: Option[String])
+
+object ValidationResult {
+  
+  def valid: ValidationResult = ValidationResult(isValid = true, None)
+  
+  def invalid(validationError: String): ValidationResult = ValidationResult(isValid = false, Some(validationError))
+  
+}
 
 
 
